2090b4daacf1c53e4a46f2c3d08fd82400b0d275
Danillo Araki <danillo.araki@hotmail.com>
2023-06-08 10:57:22 -0300

    utilizando let variavel da aula 03 JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,9 @@
-alert("Jesus é o Salvador");
-confirm("Deseja aceitar Jesus como seu unico e salvador?")
-prompt("Digite sua idade")
+// alert("Jesus é o Salvador");
+
+// let retornoDoUsuario =  confirm("Deseja aceitar Jesus como seu unico e salvador?")
+
+// console.log(retornoDoUsuario)
+
+let idade=prompt ("Digite sua idade")
+console.log(idade)
+
